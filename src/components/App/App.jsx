b8457375b242956e3ab02de15f6df9c0823275c0
oleg <hnapovskiy20@gmail.com>
2019-10-17 21:51:30 +0300

    ,

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './styles.css';
-// @ts-ignore
 import Header from '../Header/Header.tsx';
 
 export class App extends React.Component {
--- src/components/App/AppContainer.js
+++ src/components/App/AppContainer.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import * as todoOperations from '../../redux/todoCards/todoOperations';
 import fetchProfile from '../../redux/profile/profileOperations';
+// eslint-disable-next-line import/no-unresolved
 import App from './App.tsx';
 
 const mapDispatchToProps = {
